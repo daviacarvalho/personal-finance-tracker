907682257767537b9858e71fdcaba3041adccdd8
DaviAieta <aieta.davi@gmail.com>
2025-03-25 17:27:32 -0600

    starting creating user service

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get()
   getUsers() {
-    return 'get users';
+    return this.usersService.getUsers();
+  }
+
+  @Post('/create')
+  createUser(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto);
+    return this.usersService.createUser(createUserDto);
   }
 }
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Body,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  Post,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+  async getUsers() {
+    const users = await this.prisma.user.findMany();
+    if (!users || users.length == 0) {
+      throw new HttpException('No users found', HttpStatus.NOT_FOUND);
+    }
+    return users;
+  }
+
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          name: createUserDto.name,
+          email: createUserDto.email,
+          password: createUserDto.password,
+        },
+      });
+
+      return user;
+    } catch (err) {
+      console.log(err);
+      throw new HttpException(
+        'Impossible to create the user',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+}
